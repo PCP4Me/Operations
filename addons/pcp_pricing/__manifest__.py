48525246910f6aa22f2ff4cea5deb0177269d371
Marc <marc.malloy@pcp4me.com>
2025-10-21 13:55:16 -0400

    Add files via upload

---
+++ addons/pcp_pricing/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "PCP4me Pricing & Remittance",
+    "version": "19.0.1.0.0",
+    "summary": "Quoting, census diff, PMPM pricing, provider remittance",
+    "category": "Sales",
+    "author": "Sevenya",
+    "license": "OEEL-1",
+    "depends": ["base", "mail", "sale_management", "contacts", "documents"],
+    "data": [
+        "data/groups.xml",
+        "security/pcp_security.xml",
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+        "views/wizards.xml"
+    ],
+    "application": True,
+    "installable": True
+}
